04028b768b6828148a1835a59327854d90317fb7
Tanzeel Khan <ktanzeel80@gmail.com>
2021-06-19 12:23:14 +0530

    Fixing bugs

--- index.php
+++ index.php
@@ -1,1 +1,34 @@
-<p>Hello</p>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<meta name="viewport" content="width=device-width" initial-scale=1>
+<link rel="stylesheet" type="text/css" href="public/css/style.css">
+	<title>Ct₹l Budget</title>
+</head>
+<body>
+	<?php
+	include 'includes/header.php';
+	?>
+	<div class="row">
+		<div class="col-xs-12">
+			<div id="banner-img">
+				<div class="container">
+					<center>
+						<div id="banner-content">
+							<h1>We help you to control your budget.</h1><br>
+							<a href="templates/login.php" class="btn btn-info btn-lg active">Start Today</a>
+						</div>
+					</center>
+				</div>
+			</div>
+		</div>
+	</div>
+	<?php
+	include 'includes/footer.php';
+	?>
+</body>
+</html>
